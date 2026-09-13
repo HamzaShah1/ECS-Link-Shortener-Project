41d575deadb7ea97f74b7e1d4acf9e148e2f9a90
Hamza <hamzashah02@outlook.com>
2026-09-13 14:52:52 +0100

    created ECS infrastructure

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -130,3 +130,83 @@ resource "aws_ecs_task_definition" "worker" {
     }
   ])
 }
+
+resource "aws_ecs_service" "api" {
+  name            = "api"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.subnet_2.id,
+      aws_subnet.subnet_4.id
+    ]
+
+    security_groups = [
+      aws_security_group.ecs_security_group.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "api"
+    container_port   = 8080
+  }
+}
+
+
+resource "aws_ecs_service" "dashboard" {
+  name            = "dashboard"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.dashboard.arn
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.subnet_2.id,
+      aws_subnet.subnet_4.id
+    ]
+
+    security_groups = [
+      aws_security_group.ecs_security_group.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.dashboard.arn
+    container_name   = "dashboard"
+    container_port   = 8081
+  }
+}
+
+resource "aws_ecs_service" "worker" {
+  name            = "worker"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.worker.arn
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.subnet_2.id,
+      aws_subnet.subnet_4.id
+    ]
+
+    security_groups = [
+      aws_security_group.ecs_security_group.id
+    ]
+
+    assign_public_ip = false
+  }
+
+}
