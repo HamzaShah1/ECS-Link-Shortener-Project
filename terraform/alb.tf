26f03cb1c6192d9140dc5a2ed141c9b14735d9af
Hamza <hamzashah02@outlook.com>
2026-09-13 16:16:05 +0100

    assed ALB listeners and ECS services

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -12,6 +12,10 @@ resource "aws_lb_target_group" "api" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
   target_type = "ip"
+
+  health_check {
+    path = "/healthz"
+  }
 }
 
 resource "aws_lb_target_group" "dashboard" {
@@ -20,9 +24,46 @@ resource "aws_lb_target_group" "dashboard" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
   target_type = "ip"
+
+  health_check {
+    path = "/healthz"
+  }
 }
 
 resource "aws_acm_certificate" "certificate" {
   domain_name       = "hamza-aws-project.com"
   validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.hamza-aws-project.com"
+  ]
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  certificate_arn = aws_acm_certificate.certificate.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "dashboard" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  condition {
+    host_header {
+      values = ["dashboard.hamza-aws-project.com"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dashboard.arn
+  }
 }
