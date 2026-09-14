1f5e1212265fd2dae0e36b0225de1d120c523f77
Hamza <hamzashah02@outlook.com>
2026-09-14 14:03:08 +0100

    moved terraform state to remote state - s3 bucket

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "hamza-aws-project-url-shortener-terraform-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = "true"
+    use_lockfile = "true"
+  }
 }
 
 #configure the AWS provider
